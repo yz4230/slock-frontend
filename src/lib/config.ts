cdcaeff78a620de11631199d2b1777bb0b893601
Ishiyama Yuzuki <yuzupon1480@gmail.com>
2025-07-03 20:15:09 +0900

    fix: update config parsing to explicitly use VITE_API_BASE_URL from environment variables

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,6 +4,8 @@ const ConfigSchema = object({
 	API_BASE_URL: string(),
 });
 
-const config = parse(ConfigSchema, import.meta.env);
+const config = parse(ConfigSchema, {
+	API_BASE_URL: import.meta.env.VITE_API_BASE_URL,
+});
 
 export default config;
