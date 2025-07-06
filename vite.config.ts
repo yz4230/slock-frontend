97d31fb4fa908cab34fa655b38f22deb8cacbdc5
Ishiyama Yuzuki <yuzupon1480@gmail.com>
2025-07-07 01:04:33 +0900

    dev: add babel-plugin-react-compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-    tanstackRouter({ autoCodeSplitting: true }),
-    viteReact(),
-    tailwindcss(),
-    tsconfigPaths(),
-  ],
-})
+	plugins: [
+		tanstackRouter({ autoCodeSplitting: true }),
+		viteReact({ babel: { plugins: ["babel-plugin-react-compiler"] } }),
+		tailwindcss(),
+		tsconfigPaths(),
+	],
+});
